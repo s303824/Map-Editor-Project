c8378184bb47a7ee41a7e5995efebd9656a57b5e
guarm22 <60443541+guarm22@users.noreply.github.com>
2022-10-12 19:33:52 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client/'));
+    app.use(express.static('client'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
