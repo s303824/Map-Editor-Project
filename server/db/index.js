e62c0144054c8c204aeb21f46fffcd7bdb55efe1
guarm22 <60443541+guarm22@users.noreply.github.com>
2022-10-12 18:25:42 -0400

    heroku

--- server/db/index.js
+++ server/db/index.js
@@ -5,7 +5,6 @@ dotenv.config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = "mongodb+srv://silonyx:tornado123@cluster0.qhqnp4y.mongodb.net/test?retryWrites=true&w=majority";
-const test = "mongodb://silonyx:tornado123@cluster0.qhqnp4y.mongodb.net:27017/"
 const db = mongoose.connect(uri)  
 
 
