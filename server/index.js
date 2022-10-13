ce8e5504cca6be9f4e7e2f7e045ef4ff61a2f19a
guarm22 <60443541+guarm22@users.noreply.github.com>
2022-10-12 20:27:53 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
     console.log(path.resolve('..', 'client', 'build', 'index.html') + "\n")
   
     // Express serve up index.html file if it doesn't recognize route
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
       res.sendFile(path.resolve('..', 'client', 'build', 'index.html'));
     });
   
