3ed4fac07d20a5612261f365dd7f9fc7d93dcc2a
guarm22 <60443541+guarm22@users.noreply.github.com>
2022-10-12 20:00:40 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
+const path = require("path");
 
 // CREATE OUR SERVER
 dotenv.config()
