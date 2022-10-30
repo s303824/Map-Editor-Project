886245ebb97dc3039b127beb0b06398653187356
guarm22 <60443541+guarm22@users.noreply.github.com>
2022-10-30 13:24:31 -0400

    accidentally deleted a file

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Banner from "./components/navbar.component";
+import './App.css';
+import { GlobalStoreContextProvider } from './store'
+import { AuthContextProvider } from "./auth";
+import Home from "../src/routes/Home";
+import MapEditor from "./routes/MapEditor";
+import MapViewer from "./routes/MapViewer";
+import MyProjects from "./routes/MyProjects";
+
+
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <GlobalStoreContextProvider>  
+          <Banner/>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/editor" element={<MapEditor/>}/>
+              <Route path="/view" element={<MapViewer/>}/>
+              <Route path="/projects" element={<MyProjects/>}/>
+            </Routes>
+        </GlobalStoreContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
