b42cffbc237e6f24ce5746de73a09116e36a5899
Arpina1 <nazifeburcu.acikoz@stonybrook.edu>
2022-10-30 13:12:54 -0400

    updated front-end routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "../src/routes/Home";
 import Banner from "./components/navbar.component";
+import MapEditor from "../src/routes/MapEditor";
+import MapToolBar from "../src/components/map-toolbar.component";
+import MyProjects from "./routes/MyProjects";
+import MapViewer from "./routes/MapViewer";
+import SearchResults from "./routes/SearchResults";
 import './App.css';
 import { GlobalStoreContextProvider } from './store'
 import { AuthContextProvider } from "./auth";
-
-
-
+ 
 const App = () => {
 
   return (
@@ -17,6 +20,9 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/home" element={<Home/>}/>
+              <Route path="/mapeditor" element={<MapEditor/>}/>
+              <Route path="/myprojects" element={<MyProjects/>}/>
+              <Route path="/mapviewer" element={<MapViewer/>}/>
             </Routes>
         </GlobalStoreContextProvider>
       </AuthContextProvider>
