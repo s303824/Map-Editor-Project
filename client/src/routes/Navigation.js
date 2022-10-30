e15086816b62adf2169f2980a5f4af6d275db57c
Arpina1 <nazifeburcu.acikoz@stonybrook.edu>
2022-10-30 15:09:56 -0400

    updated navigation bar for all components

--- client/src/routes/Navigation.js
+++ client/src/routes/Navigation.js
@@ -3,13 +3,21 @@ import "../App.css"
 import {Route, Link, Routes, useLocation} from 'react-router-dom';
 import MapToolBar from '../components/map-toolbar.component';
 import Banner from '../components/navbar.component';
+import TileSetToolBar from '../components/tileset-editor-toolbar.component';
 
 const Navigation=() =>{ 
     const location = useLocation();
+    const pathname = location.pathname;
+    let toolbar =
+    pathname === "/mapeditor"
+    ? <MapToolBar/>
+    : pathname === "/tileseteditor"
+    ? <TileSetToolBar/>
+    : <Banner/>;
 
     return (
       <Box>
-        {location.pathname =="/mapeditor" ? <MapToolBar/> : <Banner/>}
+        {toolbar}
       </Box>
     );
   }
