8bc41d35e56f87446ab5803a6fa1b1bfd93cd1c8
guarm22 <60443541+guarm22@users.noreply.github.com>
2022-12-04 15:21:07 -0500

    excalidraw test

---
+++ client/src/components/excalidraw-test.component.js
@@ -0,0 +1,115 @@
+import { Button, Grid } from '@mui/material';
+import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react'
+import { GlobalStoreContext } from '../store'
+import "../App.css"
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import AuthContext from '../auth';
+import {Excalidraw} from "@excalidraw/excalidraw"
+import { useRef } from 'react';
+import initalData from "./initalData"
+
+function ExcalidrawTest() {
+
+    const excalidrawRef = useRef(null);
+
+  const [viewModeEnabled, setViewModeEnabled] = useState(false);
+  const [zenModeEnabled, setZenModeEnabled] = useState(false);
+  const [gridModeEnabled, setGridModeEnabled] = useState(false);
+
+  const updateScene = () => {
+    const sceneData = {
+      elements: [
+        {
+          type: "rectangle",
+          version: 141,
+          versionNonce: 361174001,
+          isDeleted: false,
+          id: "oDVXy8D6rom3H1-LLH2-f",
+          fillStyle: "hachure",
+          strokeWidth: 1,
+          strokeStyle: "solid",
+          roughness: 1,
+          opacity: 100,
+          angle: 0,
+          x: 100.50390625,
+          y: 93.67578125,
+          strokeColor: "#c92a2a",
+          backgroundColor: "transparent",
+          width: 186.47265625,
+          height: 141.9765625,
+          seed: 1968410350,
+          groupIds: [],
+        },
+      ],
+      appState: {
+        viewBackgroundColor: "#edf2ff",
+      },
+    };
+    excalidrawRef.current.updateScene(sceneData);
+  };
+
+  return (
+    <Box className="App" sx={{height:"1000px"}}>
+      <h1> Excalidraw Example</h1>
+      <div className="button-wrapper">
+        <button className="update-scene" onClick={updateScene}>
+          Update Scene
+        </button>
+        <button
+          className="reset-scene"
+          onClick={() => {
+            excalidrawRef.current.resetScene();
+          }}
+        >
+          Reset Scene
+        </button>
+        <label>
+          <input
+            type="checkbox"
+            checked={viewModeEnabled}
+            onChange={() => setViewModeEnabled(!viewModeEnabled)}
+          />
+          View mode
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={zenModeEnabled}
+            onChange={() => setZenModeEnabled(!zenModeEnabled)}
+          />
+          Zen mode
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={gridModeEnabled}
+            onChange={() => setGridModeEnabled(!gridModeEnabled)}
+          />
+          Grid mode
+        </label>
+      </div>
+      <Box className="excalidraw-wrapper" sx={{marginLeft:30, height:1000}}>
+        <Excalidraw
+          ref={excalidrawRef}
+          initialData={initalData}
+          onChange={(elements, state) =>
+            console.log("Elements :", elements, "State : ", state)
+          }
+          onPointerUpdate={(payload) => console.log(payload)}
+          onCollabButtonClick={() =>
+            window.alert("You clicked on collab button")
+          }
+          viewModeEnabled={viewModeEnabled}
+          zenModeEnabled={zenModeEnabled}
+          gridModeEnabled={gridModeEnabled}
+        />
+      </Box>
+    </Box>
+  );
+
+  }
+  
+  export default ExcalidrawTest;
